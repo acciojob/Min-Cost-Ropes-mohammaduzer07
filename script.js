a4f5e323d0b69d3f929ed1b200009620e5611c40
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2024-04-10 07:30:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,26 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	let pq = [];
+
+	for(let i = 0; i < arr.length; i++){
+		pq.push(arr[i]);
+	}
+	pq.sort(function (a, b) {
+		return a-b;
+	});
+	let minSum = 0;
+	while(pq.length > 1){
+		let first = pq.shift();
+		let second = pq.shift();
+
+		minSum += first + second;
+		pq.push(first+second);
+		pq.sort(function (a, b) {
+			return a- b;
+		});
+	}
+	return minSum;
 }
 
 module.exports=mincost;
